26727d335b618f1ab53445bf4e8a6e436c3ba025
Rigellute <rigellute@gmail.com>
2019-06-18 22:37:56 +0100

    Increase height of Header

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,6 +24,13 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const Body = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -40,21 +47,14 @@ const Layout = ({ children }) => (
         <>
           <GlobalStyle />
           <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
+          <Body>
             <main>{children}</main>
             <footer>
               © {new Date().getFullYear()}, Built with
               {` `}
               <a href="https://www.gatsbyjs.org">Gatsby</a>
             </footer>
-          </div>
+          </Body>
         </>
       </ThemeProvider>
     )}
