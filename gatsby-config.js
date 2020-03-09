43ac6716d55a4b3fb9ca75b8e6cc03e7f12a8a27
Alexander Keliris <rigellute@gmail.com>
2020-03-09 16:14:47 +0000

    Whitelist code from purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,9 +127,9 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
+        develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: ['img'], // Don't remove this selector
+        whitelist: ['img', 'code'], // Don't remove this selector
         ignore: ['prismjs/', 'rigel-prism.css'], // Ignore files/folders
       },
     },
