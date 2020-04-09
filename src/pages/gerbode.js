675cfa822b39574ffa72226fbf99db089fa3b5d0
Alexander Keliris <rigellute@gmail.com>
2020-04-09 14:41:21 +0100

    Fix class keyword and improve gerbode search

--- src/pages/gerbode.js
+++ src/pages/gerbode.js
@@ -2,9 +2,10 @@ import React from 'react';
 import MeiliSearch from 'meilisearch';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import SearchIcon from '../images/search.inline.svg';
 
 const meili = new MeiliSearch({
-  host: 'https://gerbode.rigellute.com',
+  host: 'http://gerbode.rigellute.com',
   apiKey: '06aea15cdfe8e997d057bf3a38f1663189b352026cebed8e0aae0767d89632be',
 });
 
@@ -35,19 +36,28 @@ export default function Gerbode() {
       <h1>Search Lute Music</h1>
       <p>
         This index contains more than 8000 lute pieces compiled by{' '}
-        <a href="http://www.gerbode.net/">Sarge Gerbode</a>.
+        <a href="http://www.gerbode.net/">Sarge Gerbode</a>. Start typing the
+        composer or title to get results e.g. "Flow my".
       </p>
-      <input
-        type="text"
-        placeholder="John Dowland"
-        className="rounded my-4 p-2 w-full"
-        value={inputValue}
-        onChange={e => updateInput(e.target.value)}
-      />
+      <div className="mb-5 relative">
+        <input
+          type="text"
+          placeholder="Search Gerbode..."
+          className="transition-colors duration-100 ease-in-out focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-200 py-2 pr-4 pl-10 block w-full appearance-none leading-normal ds-input"
+          value={inputValue}
+          onChange={e => updateInput(e.target.value)}
+        />
+        <div className="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+          <SearchIcon className="fill-none pointer-events-none text-gray-600 w-4 h-4" />
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
         {searchResults.hits.map(result => (
-          <div class="flex flex-col justify-between w-full bg-white rounded shadow-lg mb-4">
-            <div class="px-6 py-4">
+          <div
+            key={result.id}
+            className="flex flex-col justify-between w-full bg-white rounded shadow-lg mb-4"
+          >
+            <div className="px-6 py-4">
               <h3>
                 {result.composer} - {result.title}
               </h3>
@@ -61,14 +71,14 @@ export default function Gerbode() {
                 Date: <span className="font-semibold">{result.date}</span>
               </div>
             </div>
-            <div class="px-6 py-4">
-              <span class="inline-block py-1 text-sm font-semibold mr-5">
+            <div className="px-6 py-4">
+              <span className="inline-block py-1 text-sm font-semibold mr-5">
                 <a href={result.pdf}>PDF</a>
               </span>
-              <span class="inline-block py-1 text-sm font-semibold mr-5">
+              <span className="inline-block py-1 text-sm font-semibold mr-5">
                 <a href={result.midi}>MIDI</a>
               </span>
-              <span class="inline-block py-1 text-sm font-semibold mr-5">
+              <span className="inline-block py-1 text-sm font-semibold mr-5">
                 <a href={result.fromino}>fromino</a>
               </span>
             </div>
