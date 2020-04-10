aa55f57714c9dff5891d47974fd6c20a49a011bf
Alexander Keliris <rigellute@gmail.com>
2020-04-10 11:06:12 +0100

    Fix nav icon margin

--- src/components/icon-link.js
+++ src/components/icon-link.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Github from '../images/github.inline.svg';
 import Twitter from '../images/twitter.inline.svg';
 
-export const IconLink = ({ href, Icon, isInverted }) => {
+export const IconLink = ({ href, Icon, isInverted, className }) => {
   const hoverColor = isInverted ? 'text-gray-300' : 'text-gray-700';
   return (
     <a
-      className={`block flex items-center text-gray-500 hover:${hoverColor} mr-5`}
+      className={`${className} block flex items-center text-gray-500 hover:${hoverColor}`}
       href={href}
       target="_blank"
       rel="noopener noreferrer"
