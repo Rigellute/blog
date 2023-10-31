817dd36da6af6c83dec880f01a0c74249d158406
Alexander Keliris <rigellute@gmail.com>
2023-10-31 16:07:09 +0000

    Add articles page

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,27 +1,2 @@
-import { formatDate } from "../utils";
-import { Card } from "./Card";
-
-interface Article {
-  title: string;
-  description: string;
-  date: Date;
-}
-
-export interface ArticleWithSlug extends Article {
-  slug: string;
-}
-
-export function Article({ article }: { article: ArticleWithSlug }) {
-  return (
-    <Card as="article">
-      <Card.Title href={`/articles/${article.slug}`}>
-        {article.title}
-      </Card.Title>
-      <Card.Eyebrow as="time" dateTime={article.date.toISOString()} decorate>
-        {formatDate(article.date)}
-      </Card.Eyebrow>
-      <Card.Description>{article.description}</Card.Description>
-      <Card.Cta>Read article</Card.Cta>
-    </Card>
-  );
-}
+../../utils
+../Card
