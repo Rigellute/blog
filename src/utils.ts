346cf0c76b9764257b47937ae56ff6239ea30165
Alexander Keliris <rigellute@gmail.com>
2023-10-31 15:15:18 +0000

    Finish migrating content

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export function formatDate(date: Date) {
-  return date.toLocaleDateString(undefined, {
+  return date.toLocaleDateString('en-us', {
     day: "numeric",
     month: "long",
     year: "numeric",
