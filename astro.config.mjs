c3d260adc2157ad1a9e8c927a594c12e54a15b84
Alexander Keliris <rigellute@gmail.com>
2023-11-07 17:24:42 +0000

    Add prism highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import rehypePrism from '@mapbox/rehype-prism'
 
-import alpinejs from "@astrojs/alpinejs";
+import alpinejs from '@astrojs/alpinejs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind(), alpinejs()]
-});
+  markdown: {
+    syntaxHighlight: 'prism',
+    rehypePlugins: [rehypePrism],
+  },
+  site: 'https://keliris.dev',
+  integrations: [mdx(), sitemap(), react(), tailwind(), alpinejs()],
+})
+
