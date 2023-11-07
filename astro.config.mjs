7eb84582502025a39164b024075c8e2846f50e2e
Alexander Keliris <rigellute@gmail.com>
2023-11-07 16:32:24 +0000

    Add astro and fix header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
-})
+  integrations: [mdx(), sitemap(), react(), tailwind(), alpinejs()]
+});
