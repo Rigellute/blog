5756f28a58d518c7e1c8e9bdae072f35afae214a
Alexander Keliris <rigellute@gmail.com>
2025-02-22 13:07:53 +0000

    Revert "Upgrade astro and tailwind (#80)"
    This reverts commit dd806a756af83342465a6a80708056ea1a69685d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 import syntaxTheme from './syntax-theme.json'
-
-import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,17 +12,10 @@ export default defineConfig({
       theme: syntaxTheme,
     },
   },
-
   prefetch: {
     defaultStrategy: 'hover',
     prefetchAll: true,
   },
-
   site: 'https://keliris.dev',
-  integrations: [mdx(), sitemap(), react()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 })
-
